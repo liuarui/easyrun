edcc4f94c0074defeb6a0170b1b69c196200a92b
liuarui <liuwenrui597@qq.com>
2021-07-11 23:07:50 +0800

    feat(项目): 改为使用`cac`构建命令行

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
-import { terser } from 'rollup-plugin-terser'
 export default {
     input: 'src/main.ts',
     output: {
@@ -12,5 +11,5 @@ export default {
         format: 'umd',
     },
     external: [],
-    plugins: [resolve(), commonjs(), json(), typescript(), terser()],
+    plugins: [resolve(), commonjs(), json(), typescript()],
 }
